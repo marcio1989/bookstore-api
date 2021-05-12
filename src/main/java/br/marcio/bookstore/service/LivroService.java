eb95e8748649fe0aeb05a57340beacecd383612d
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-12 10:05:53 -0300

    POST para livro

--- src/main/java/br/marcio/bookstore/service/LivroService.java
+++ src/main/java/br/marcio/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.marcio.bookstore.domain.Categoria;
 import br.marcio.bookstore.domain.Livro;
 import br.marcio.bookstore.repositories.LivroRepository;
 import br.marcio.bookstore.service.exceptions.ObjectNotFoundException;
@@ -41,5 +42,12 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
