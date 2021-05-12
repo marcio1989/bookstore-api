b171ab284f16cd71b9d476d0b229cb3902eea83d
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-11 23:01:47 -0300

    PUT para Categoria

--- src/main/java/br/marcio/bookstore/service/CategoriaService.java
+++ src/main/java/br/marcio/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.marcio.bookstore.domain.Categoria;
+import br.marcio.bookstore.dto.CategoriaDTO;
 import br.marcio.bookstore.repositories.CategoriaRepository;
 import br.marcio.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -27,6 +28,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
